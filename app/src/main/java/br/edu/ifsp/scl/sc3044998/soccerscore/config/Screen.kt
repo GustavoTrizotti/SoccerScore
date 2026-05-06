0a4794bb5c2976661e40bc40ffd84d9362548ade
Gustavo Trizotti <gustavo.trizotti@aluno.ifsp.edu.br>
2026-05-05 21:13:00 -0300

    refac: remove old screen routing sealed class

--- app/src/main/java/br/edu/ifsp/scl/sc3044998/soccerscore/config/Screen.kt
+++
@@ -1,7 +0,0 @@
-package br.edu.ifsp.scl.sc3044998.soccerscore.config
-
-sealed class Screen(val route: String) {
-    object MatchSettings : Screen("match_settings_screen");
-    object MatchSummary : Screen("match_summary_screen");
-    object MatchResult : Screen("match_result_screen");
-}
